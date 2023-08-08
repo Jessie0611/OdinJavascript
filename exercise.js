5ea3568c75d6625a0ec85083767396ea6a7c1c6b
Jessie0611~ <imjustme0611@hotmail.com>
2023-08-08 19:01:56 -0400

    New practices

--- exercise.js
+++ exercise.js
@@ -2,3 +2,37 @@ var lbs = prompt("Weight in pounds (lbs)?");
 var kg = lbs*0.454;
 alert("That equals: "+kg+" kilograms");
 console.log("Conversion Complete!");
+
+//Operators Exercise
+// === instead of ==
+// && (and) || (or)  !(not)  !== (not equal to)
+var x = 1
+var y = 2
+
+//exercise1
+"2" == y && x === 1;
+//return true
+//exercise2
+x >= 0 || y === "2";
+//return true 
+//exercise3
+!(x !== 1) && y === (1+1);
+//return true
+//exercise4
+y !== "2" && x < 10;
+//return true
+
+//Control Flow Exercise
+//IF statment
+// if(condition){
+    //Execute code
+// }else{
+    //Execuite some other code
+//IF, ELSE IF, ELSE statement 
+// if (condition one){
+    //Execute code
+// }else if(condition two){
+    //Execute other code
+// }else{
+    //execute backup code
+// }
--- script.js
+++ script.js
@@ -24,3 +24,16 @@ var name = "Jessie";
 alert(greeting+name);
 
 console.log("Hey I'm in the console.");
+//console.log() is the command to print something to the developer console (browser)
+//use to print the results from following articles and exercises to the console
+
+//     \n start a new line
+//     \t gives me a tab over
+//mod or modulo is a percent sign "%" and gives you the remainder of a math equation
+//Scope - When you create a function, the variables and other things
+// defined inside the function are inside their own separate scope,
+//meaning that they are locked away in their own separate compartments,
+//unreachable from code outside the functions. 
+//The top level outside all your functions is called the global scope.
+//Values defined in the global scope are accessible from everywhere in the code
+   
