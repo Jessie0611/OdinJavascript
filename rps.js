3ee30a1be07c780fd689fceb1e139fd3e4b63569
Jessie0611~ <imjustme0611@hotmail.com>
2023-10-25 13:35:35 -0400

    Practice

--- rps.js
+++ rps.js
@@ -1,23 +1,28 @@
-let userPrompt = prompt("Choose: Rock, Paper, Scissors?")
-
-function getComputerChoice(){
-    let randomNum = Math.floor(Math.random()*3)
-    let compPlay
-    if (randomNum === 0){
-        compPlay = "Rock"
-    } else if (randomNum === 1){
-        compPlay = "Paper"
-    } else{
-        compPlay = "Scissors"
+function returnGuess(int) {
+    let val;
+    if (int === 0) {
+        val = "Rock";
+    } else if (int === 1) {
+        val = "Paper";
+    } else {
+        val = "Scissors";
     }
-    return compPlay
+    console.log(`Computer has chosen ${val}`);
+    return val;
 }
-let compPlay = getComputerChoice()
 
-if (compPlay === 0 && userPrompt === 1){
-    console.log("You lose, rock beats paper!")
-}else (compPlay === 1 && userPrompt === 2){
-    console.log("You win! Scissors beats paper!")
+const randomNum = Math.floor(Math.random() * 3);
+const computerGuess = returnGuess(randomNum);
+const userGuess = prompt("Select Rock, Paper, or Scissors");
+
+if (computerGuess === userGuess) {
+    console.log("Tie");
+} else if ((userGuess === "Rock" && computerGuess === "Scissors") || 
+    (userGuess === "Scissors" && computerGuess === "Paper") ||
+    (userGuess === "Paper" && computerGuess === "Rock")) {
+        console.log("User Wins");
+} else {
+    console.log("Computer Wins");
 }
 
 
