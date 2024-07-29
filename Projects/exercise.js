341169a8a5632ba342fbd43e70b1d59b11353dba
Jessie0611~ <imjustme0611@hotmail.com>
2024-07-29 19:27:35 -0400

    Add notes

--- Projects/exercise.js
+++ Projects/exercise.js
@@ -36,3 +36,54 @@ y !== "2" && x < 10;
 // }else{
     //execute backup code
 // }
+
+// your JavaScript file
+const container = document.querySelector("#container");
+
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "Multiple JS exercises will consist on this page!";
+
+container.appendChild(content);
+
+
+const btn = document.querySelector("#btn");
+btn.onclick = () => alert("Hello World");
+btn.addEventListener("click", function (e) {
+    console.log(e.target);
+  });
+  
+  btn.addEventListener("click", function (e) {
+    e.target.style.background = "blue";
+  });
+  
+
+  // buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    alert(button.id);
+  });
+});
+
+//Event Bubbling: An event flows from target element up to the root element.
+//Event Capturing:An event flows from the root element down to the target element.
+//event.preventDefault() = method used to prevent the default action of an event.
+//event.stopPropagation() = method used to stop an event from propagating to parent element.
+//event.stopImmediatePropagation() = method to stop event from being handled by any other handlers in the current target.
+//Event handler is a function that is called when an event occurs.
+//To add event listener for a click event button: button.addEventListener('click', function() {...});
+
+//What will the following code do?
+document.getElementById('myLink').addEventListener('click', function(event) {
+    event.preventDefault();
+    alert('Link clicked!');
+  });
+//The default action of the link will be prevented, and an alert will be shown
+
+//In which phase is the event target itself handled in event propagation? Target phase
+//What is the correct order of event phases in the W3C model? Capturing phase, Target phase, Bubbling phase
+
